32fafed3c69422b757667436f690d36ff7a7941a
hughjph <hughjohnson2018@outlook.com>
2021-11-30 11:00:20 +0000

    intial commit

---
+++ MathsEngine/Differentation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathsEngine
+{
+    public class Differentiation
+    {
+        public static void Differentiate()
+        {
+            Console.WriteLine("Maths init");
+        }
+    }
+    
+}
